5fafdd0f07de487760c4fa98d4e588ea5dfa0c54
tapisky <jctapiador@gmail.com>
2022-06-20 14:03:43 +0200

    Use the new initialize method and check for login denied or granted in the network response

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -5,18 +5,22 @@ describe('Login Page Case', () => {
   const login = new LoginPage;
 
   beforeEach(() => {
-    login.navigate();
+    login.initialize();
+    cy.intercept('POST', '/api/login').as('login');
   });
 
   it('Login with wrong username/password', () => {
     login.fillAndSendLoginInfo('something@something.com', 'somepassword');
-    cy.url().should('be.equal', 'https://app.laserhub.com/login')
+    cy.wait('@login').its('response.statusCode').should('eq', 401);
   })
 
   // For the purpose of this task, username and password are mentioned in the lines below
   // Ideally, those should be hidden under a config file ignored by git or other means
   it('Login with proper username/password', () => {
     login.fillAndSendLoginInfo('e.ioannidis+testing_worktask@laserhub.com', 'l0vet3sting@');
-    cy.url().should('not.be.equal', 'https://app.laserhub.com/login')
+    cy.wait('@login').then((interception) => {      
+      expect(interception.response.statusCode).to.eq(200);
+      expect(interception.response.body).to.have.property('redirectURL', '/product');
+    })
   })
 })
