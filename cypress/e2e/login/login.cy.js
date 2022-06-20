9530d85038317a6954f925ef34af3d99fc6c141c
tapisky <jctapiador@gmail.com>
2022-06-20 11:12:56 +0200

    Use LoginPage object's re-usable method to simplyfy and clean up cases

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -9,18 +9,14 @@ describe('Login Page Case', () => {
   });
 
   it('Login with wrong username/password', () => {
-    login.enterUsername('something@something.com');
-    login.enterPassword('somepassword');
-    login.submit();
+    login.fillAndSendLoginInfo('something@something.com', 'somepassword');
     cy.url().should('be.equal', 'https://app.laserhub.com/login')
   })
 
   // For the purpose of this task, username and password are mentioned in the lines below
   // Ideally, those should be hidden under a config file ignored by git or other means
   it('Login with proper username/password', () => {
-    login.enterUsername('e.ioannidis+testing_worktask@laserhub.com');
-    login.enterPassword('l0vet3sting@');
-    login.submit();
+    login.fillAndSendLoginInfo('e.ioannidis+testing_worktask@laserhub.com', 'l0vet3sting@');
     cy.url().should('not.be.equal', 'https://app.laserhub.com/login')
   })
 })
